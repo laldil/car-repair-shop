cccb04b5ed984e85e1ee17576805b9a1c676df52
Aldiyar <aldiar001122@gmail.com>
2025-06-06 18:52:47 +0500

    chore: initial commit

---
+++ src/test/java/com/example/car_repair_shop/CarRepairShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.car_repair_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRepairShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
